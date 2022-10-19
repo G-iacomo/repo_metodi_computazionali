f3f559c034d3d1e9492cb0da92470bfa765ceb10
G-iacomo <gb980061@infolab01>
2022-10-19 18:06:22 +0200

    esercizio 1 19/10/2022

--- es221019.py
+++ es221019.py
@@ -1,10 +1,16 @@
 import numpy as np
+import scipy
+from scipy import integrate
 import matplotlib.pyplot as plt
 t=np.arange(-5,5,0.5)
 v=np.ones(20)
+s=np.zeros(20)
 for i in range(len(t)):
-    #print (t[i])
     v[i]=(t[i]**2)-2
-    #print (v[i])
-plt.plot(t,v)
+    s[i]=scipy.integrate.simpson(v,t)
+#plt.plot(t,v)
+#plt.show()
+for i in range(len(t)):
+    print (s[i])
+plt.plot(t,s)
 plt.show()
