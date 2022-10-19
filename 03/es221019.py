257a4b2fae53608d057b8f2916ef7f4782f6645a
giacomo bartolini <giacomo.barto@gmail.com>
2022-10-19 22:53:03 +0200

    es221019

--- 03/es221019.py
+++ 03/es221019.py
@@ -2,15 +2,22 @@ import numpy as np
 import scipy
 from scipy import integrate
 import matplotlib.pyplot as plt
-t=np.arange(-5,5,0.5)
-v=np.ones(20)
-s=np.zeros(20)
-for i in range(len(t)):
-    v[i]=(t[i]**2)-2
-    s[i]=scipy.integrate.simpson(v,t)
-#plt.plot(t,v)
-#plt.show()
-for i in range(len(t)):
-    print (s[i])
-plt.plot(t,s)
+t,v=np.loadtxt('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/03/dati.txt', delimiter=',' , unpack=True)
+s=np.zeros(len(t))
+for i in range(len(t)-1):
+    s[i]=scipy.integrate.simpson(v[0:i+1],t[0:i+1])
+s[len(t)-1]=scipy.integrate.simpson(v,t)
+plt.plot(t,v,'red')
+plt.plot(t,s,'green')
 plt.show()
+
+#integrale di una funzione v arbitraria
+# t=np.arange(0.1,100,1)
+# v=np.ones(len(t))
+# s=np.zeros(len(t))
+# for i in range(len(t)-1):
+#     s[i]=scipy.integrate.simpson(v[0:i+1],t[0:i+1])
+# s[len(t)-1]=scipy.integrate.simpson(v,t)
+# plt.plot(t,v,'red')
+# plt.plot(t,s,'green')
+# plt.show()
