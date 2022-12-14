1bc14b16adf4df467f002f8cc7e0d10fd10e53b3
G-iacomo <62990189+G-iacomo@users.noreply.github.com>
2022-12-14 20:04:45 +0100

    Update reco.py

--- 09/reco.py
+++ 09/reco.py
@@ -1,28 +1,39 @@
 import numpy as np
 
+
 class Hit:
-    def __init__(self,modulo,sensore,tempo):
-        self.modulo=modulo
-        self.sensore=sensore
-        self.tempo=tempo
-    def __lt__(self,other):
-        return self.tempo < other.tempo
-    def __repr__(self):
-        return '{}'.format(self.tempo)
-    def __sub__(self,other):
-        return self.tempo - other.tempo
+
+	def __init__(self, modulo, sensore, tempo):
+		self.modulo = modulo
+		self.sensore = sensore
+		self.tempo = tempo
+
+	def __lt__(self, other):
+		return self.tempo < other.tempo
+
+	def __repr__(self):
+		return '{}'.format(self.tempo)
+
+	def __sub__(self, other):
+		return self.tempo - other.tempo
+
 
 class Event:
-    def __init__(self):
-        self.nhit=0
-        self.tstart=np.inf
-        self.tstop=-np.inf
-        self.durata=np.inf
-        self.a_Hit=np.empty(0)
-    def aggiungi_hit(self,Hit):
-        if self.nhit==0:
-            self.tstart=Hit.tempo
-        self.nhit=(self.nhit)+1
-        self.tstop=Hit.tempo
-        self.durata=self.tstop-self.tstart
-        self.a_Hit=np.append(self.a_Hit,Hit)
+
+	def __init__(self):
+		self.nhit = 0
+		self.tstart = np.inf
+		self.tstop = -np.inf
+		self.durata = np.inf
+		self.a_Hit = np.empty(0)
+
+	def aggiungi_hit(self, Hit):
+		if self.nhit == 0:
+			self.tstart = Hit.tempo
+		self.nhit = (self.nhit) + 1
+		self.tstop = Hit.tempo
+		self.durata = self.tstop - self.tstart
+		self.a_Hit = np.append(self.a_Hit, Hit)
+
+	def __repr__(self):
+		return '{}  {}   {}'.format(self.nhit, self.durata, self.a_Hit)
