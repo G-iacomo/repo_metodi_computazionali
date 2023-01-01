e1e2a87e5ba62cfc8bf781657e8a1134f335afd6
giacomo bartolini <giacomo.barto@gmail.com>
2023-01-01 23:22:20 +0100

    0b

--- e/e.py
+++ e/e.py
@@ -11,6 +11,7 @@ stati = np.array([6,8,48,46,36]) # codici degli stati analizzati.
                         #rispettivamente California, Colorado, Texas, South Dakota, New York
 check = False #se True lo script esegue piccoli controlli del corretto funzionamento in alcuni punti salienti
 pd.set_option('display.max_columns', 500) #il numero di colonne massime da stampare
+#pd.set_option('display.max_rows', 500) #il numero di righe massime da stampare
 
 ###########################################
 #              SELEZIONE STATI D'INTERESSE
@@ -25,11 +26,15 @@ if check: #controlla la presenza di eventuali dati ripetuti
     else:
         print('\nCHECK: ci sono dei duplicati\n')
 
+dati_iniziali.drop(['Unnamed: 0'], axis=1, inplace=True) #elimino la colonna senza nome. 
+            #non sono indici in quanto si ripetevano pur non essendo duplicate le righe
+dati_iniziali.reset_index(inplace=True,names='original row-2') #assegno un indice ad ogni riga. la colonna si chiama'original row-2'
+        #così tengo conto della riga di provenienza una volta elaborati i dati: riga originaria=('original row-2'+2)
 
 selezione_stati = (i in stati for i in dati_iniziali['State Code']) #maschera degli stati d'interesse: type=generator
                                         #array con indici delle righe riferite ad uno stato in 'stati'
 dati = (dati_iniziali.loc[selezione_stati]).copy() #mi assicuro di non modificare accidentalmente i dati
-dati.rename(columns={'Unnamed: 0':'original row'}, inplace=True)
+#dati.rename(columns={'Unnamed: 0':'original row'}, inplace=True)
 dati.reset_index(inplace=True)
 dati.drop(['index'], axis=1, inplace=True)
 
@@ -50,24 +55,32 @@ if check: #controlla la correttezza della selezione dei dati confrontando il num
 ##########################################
 #           SELEZIONE MEDIE
 
-indici=[] #indici da rimuovere
+#dati.to_csv('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/e/dati.csv', sep=',', index=False)
+dati.to_csv('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/e/dati.txt', sep='\t', na_rep='!!', index=False)
+
+
+indici=[] #indici da tenere
 for i in tqdm(range(0,len(dati)-4+1,4)):
-#for i in tqdm(range(262,265,4)):
+#for i in tqdm(range(0,8,4)):
     if (((dati.at[i+1,'SO2 Mean']>=0) and (dati.at[i+1,'CO Mean']>=0) )and (dati.at[i+1,'CO Mean']>=0)):
         dati.at[i+1,'SO2 Mean'] = st.fmean([dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value']])
         dati.at[i+1,'SO2 1st Max Value'] = max(dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value'])
         dati.at[i+1,'CO Mean'] = st.fmean([dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value']])
         dati.at[i+1,'CO 1st Max Value'] = max(dati.at[i+1,'CO 1st Max Value'],dati.at[i+2,'CO 1st Max Value'])
-    else:
         indici=np.append(indici,i+1)
-    indici=np.append(indici,i)
-    indici=np.append(indici,i+2)
-    indici=np.append(indici,i+3)
-    # else:
-    #   dati.drop([i+1],axis=0,inplace=True)
-    # dati.drop([i],axis=0,inplace=True)
-    # dati.drop([i+2],axis=0,inplace=True)
-    # dati.drop([i+3],axis=0,inplace=True)
 
-df = dati[~dati.index.isin(indici)]
-print(df)
+indici= indici.astype(int)
+dati = dati[dati.index.isin(indici)]
+
+# if all(dati.duplicated())==False:
+#     print('ok')
+# else:
+#     print('errore nel for')
+
+#dati.to_csv('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/e/medie_temporanee.csv', sep=',', index=False)
+dati.to_csv('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/e/medie_temporanee.txt', sep='\t', na_rep='!!', index=False)
+
+#!!!!!!! problema: alcuni valori con aqi sono invertiti eg. 158512-158516 quindi perdi valori.
+#!!!!! alcune righe dei dati originarili hanno piu di 4 rivelazione per un solo giorno! (ad occhio solo 19805)
+#!! togliere !!
+
--- e/tempCodeRunnerFile.py
+++ e/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-for i in tqdm(range(0,len(dati)-4+1,4)):
-# #for i in tqdm(range(262,265,4)):
-#     if (((dati.at[i+1,'SO2 Mean']>=0) and (dati.at[i+1,'CO Mean']>=0) )and (dati.at[i+1,'CO Mean']>=0)):
-#         dati.at[i+1,'SO2 Mean'] = st.fmean([dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value']])
-#         dati.at[i+1,'SO2 1st Max Value'] = max(dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value'])
-#         dati.at[i+1,'CO Mean'] = st.fmean([dati.at[i+1,'SO2 1st Max Value'],dati.at[i+2,'SO2 1st Max Value']])
-#         dati.at[i+1,'CO 1st Max Value'] = max(dati.at[i+1,'CO 1st Max Value'],dati.at[i+2,'CO 1st Max Value'])
-#     else:
-#         dati.drop([i+1],axis=0,inplace=True)
-#     dati.drop([i],axis=0,inplace=True)
-#     dati.drop([i+2],axis=0,inplace=True)
-#     dati.drop([i+3],axis=0,inplace=True)
+print(dati) #
