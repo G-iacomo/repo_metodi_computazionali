50d9b29ece04b416521a3602b7d4b0a2b2d03c3a
giacomo bartolini <giacomo.barto@gmail.com>
2023-01-02 20:05:10 +0100

    deleted

--- e/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-dati=e.funzione_stati()
-dati=e.funzione_medie2(dati)
-print(dati)
-
-dati.to_csv('/home/gb/Desktop/unipg/metodi_computazionali/repo_metodi_computazionali/e/medie_temporanee.txt', sep='\t', na_rep='!!', index=False)
